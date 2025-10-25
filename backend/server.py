ef9d4ef6997a63e724cacf5e51b008c3b03f8fd8
Adam Mouedden <adam.mouedden@gmail.com>
2025-10-25 09:55:43 -0400

    Major error fixed

--- backend/server.py
+++ backend/server.py
@@ -20,4 +20,4 @@ def call_gemini(prompt):
     return response.text
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", host="0.0.0.0", port="8000")
+    uvicorn.run("server:app", host="0.0.0.0", port=8000)
